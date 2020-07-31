93860e9b9e42b7155fbdf226c16f1c39a318d9fc
phuockieu <phuoc123411@gmail.com>
2020-08-01 00:23:31 +0700

    init

---
+++ api/.envs/dev/cert/command.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DOMAIN=${1:-localhost}
+
+if [[ ! -f "/files/${DOMAIN}.crt" ]]; then
+    /scripts/create-rootCA.sh
+    /scripts/create-certs.sh $DOMAIN
+fi
+
+if [[ ! -f "/files/.htpasswd" ]]; then
+    /scripts/create-htpasswd.sh
+fi
+
+if [[ ! -f "/files" ]]; then
+    mkdir /files
+fi
+
+if [[ ! -f "/files/.gitignore" ]]; then
+    echo -e "*\n!.gitignore" > /files/.gitignore
+fi
+
